63b23f866cfd3f2fe1f3b86ffeab3d00a37a45e1
BrunoMNDantas <bmndantas@gmail.com>
2019-03-04 04:15:34 +0000

    Reset

--- src/main/java/com/dantas/jscrapper/driverSupplier/IDriverSupplier.java
+++
@@ -1,9 +0,0 @@
-package com.dantas.jscrapper.driverSupplier;
-
-import org.openqa.selenium.WebDriver;
-
-public interface IDriverSupplier {
-
-    WebDriver get() throws DriverSupplierException;
-
-}
