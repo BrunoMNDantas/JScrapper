99cc079b56cb63f0272c83e6f14838169763b635
BrunoMNDantas <bmndantas@gmail.com>
2019-03-30 03:28:35 +0000

    ConfigBuilder implemented

---
+++ src/main/java/com/github/brunomndantas/jscrapper/scrapper/config/ConfigBuilder.java
@@ -0,0 +1,100 @@
+package com.github.brunomndantas.jscrapper.scrapper.config;
+
+import com.github.brunomndantas.jscrapper.core.ScrapperException;
+import com.github.brunomndantas.jscrapper.core.config.ClassConfig;
+import com.github.brunomndantas.jscrapper.core.config.FieldConfig;
+
+public class ConfigBuilder {
+
+    public static void buildConfig(ClassConfig config) throws ScrapperException {
+        buildInstanceFactory(config);
+        buildDriverSupplier(config);
+        buildDriverLoader(config);
+
+        for(FieldConfig fieldConfig : config.getFieldsConfig())
+            buildConfig(fieldConfig);
+    }
+
+    private static void buildConfig(FieldConfig config) throws ScrapperException {
+        buildDriverLoader(config);
+        buildSelector(config);
+        buildElementLoader(config);
+        buildParser(config);
+        buildProperty(config);
+    }
+
+
+    private static void buildInstanceFactory(ClassConfig config) throws ScrapperException {
+        if(config.getInstanceFactory() == null) {
+            config.setInstanceFactory(AnnotationClassConfig.getInstanceFactory(config.getKlass()));
+
+            if(config.getInstanceFactory() == null)
+                config.setInstanceFactory(DefaultClassConfig.getInstanceFactory(config.getKlass()));
+        }
+    }
+
+    private static void buildDriverSupplier(ClassConfig config) throws ScrapperException {
+        if(config.getDriverSupplier() == null) {
+            config.setDriverSupplier(AnnotationClassConfig.getDriverSupplier(config.getKlass()));
+
+            if(config.getDriverSupplier() == null)
+                config.setDriverSupplier(DefaultClassConfig.getDriverSupplier(config.getKlass()));
+        }
+    }
+
+    private static void buildDriverLoader(ClassConfig config) throws ScrapperException {
+        if(config.getDriverLoader() == null) {
+            config.setDriverLoader(AnnotationClassConfig.getDriverLoader(config.getKlass()));
+
+            if(config.getDriverLoader() == null)
+                config.setDriverLoader(DefaultClassConfig.getDriverLoader(config.getKlass()));
+        }
+    }
+
+
+    private static void buildDriverLoader(FieldConfig config) throws ScrapperException {
+        if(config.getDriverLoader() == null) {
+            config.setDriverLoader(AnnotationFieldConfig.getDriverLoader(config.getField()));
+
+            if(config.getDriverLoader() == null)
+                config.setDriverLoader(DefaultFieldConfig.getDriverLoader(config.getField()));
+        }
+    }
+
+    private static void buildSelector(FieldConfig config) throws ScrapperException {
+        if(config.getSelector() == null) {
+            config.setSelector(AnnotationFieldConfig.getSelector(config.getField()));
+
+            if(config.getSelector() == null)
+                config.setSelector(DefaultFieldConfig.getSelector(config.getField()));
+        }
+    }
+
+    private static void buildElementLoader(FieldConfig config) throws ScrapperException {
+        if(config.getElementLoader() == null) {
+            config.setElementLoader(AnnotationFieldConfig.getElementLoader(config.getField()));
+
+            if(config.getElementLoader() == null)
+                config.setElementLoader(DefaultFieldConfig.getElementLoader(config.getField()));
+        }
+    }
+
+    private static void buildParser(FieldConfig config) throws ScrapperException {
+        if(config.getParser() == null) {
+            config.setParser(AnnotationFieldConfig.getParser(config.getField()));
+
+            if(config.getParser() == null)
+                config.setParser(DefaultFieldConfig.getParser(config.getField()));
+        }
+    }
+
+    private static void buildProperty(FieldConfig config) throws ScrapperException {
+        if(config.getProperty() == null) {
+            config.setProperty(AnnotationFieldConfig.getProperty(config.getField()));
+
+            if(config.getProperty() == null)
+                config.setProperty(DefaultFieldConfig.getProperty(config.getField()));
+        }
+    }
+
+}
---
+++ src/test/java/com/github/brunomndantas/jscrapper/scrapper/config/ConfigBuilderTest.java
@@ -0,0 +1,163 @@
+package com.github.brunomndantas.jscrapper.scrapper.config;
+
+import com.github.brunomndantas.jscrapper.core.config.ClassConfig;
+import com.github.brunomndantas.jscrapper.core.config.FieldConfig;
+import com.github.brunomndantas.jscrapper.core.driverLoader.DriverLoaderException;
+import com.github.brunomndantas.jscrapper.core.driverLoader.IDriverLoader;
+import com.github.brunomndantas.jscrapper.core.driverSupplier.DriverSupplierException;
+import com.github.brunomndantas.jscrapper.core.driverSupplier.IDriverSupplier;
+import com.github.brunomndantas.jscrapper.core.elementLoader.ElementLoaderException;
+import com.github.brunomndantas.jscrapper.core.elementLoader.IElementLoader;
+import com.github.brunomndantas.jscrapper.core.instanceFactory.IInstanceFactory;
+import com.github.brunomndantas.jscrapper.core.instanceFactory.InstanceFactoryException;
+import com.github.brunomndantas.jscrapper.core.parser.IParser;
+import com.github.brunomndantas.jscrapper.core.parser.ParserException;
+import com.github.brunomndantas.jscrapper.core.property.IProperty;
+import com.github.brunomndantas.jscrapper.core.property.PropertyException;
+import com.github.brunomndantas.jscrapper.core.selector.ISelector;
+import com.github.brunomndantas.jscrapper.core.selector.SelectorException;
+import com.github.brunomndantas.jscrapper.scrapper.annotation.element.*;
+import com.github.brunomndantas.jscrapper.scrapper.annotation.page.DriverLoader;
+import com.github.brunomndantas.jscrapper.scrapper.annotation.page.DriverSupplier;
+import com.github.brunomndantas.jscrapper.scrapper.annotation.page.InstanceFactory;
+import com.github.brunomndantas.jscrapper.scrapper.annotation.page.Page;
+import com.github.brunomndantas.jscrapper.support.instanceFactory.EmptyConstructorInstanceFactory;
+import com.github.brunomndantas.jscrapper.support.parser.single.text.reference.SingleReferenceStringTextParser;
+import com.github.brunomndantas.jscrapper.support.property.FieldProperty;
+import com.github.brunomndantas.jscrapper.support.selector.IdSelector;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import static org.junit.Assert.*;
+
+public class ConfigBuilderTest {
+
+    private static class MyInstanceFactory implements IInstanceFactory {
+        @Override public Object create() throws InstanceFactoryException { return null; }
+    }
+
+    private static class MyDriverSupplier implements IDriverSupplier {
+        @Override public WebDriver get() throws DriverSupplierException { return null; }
+    }
+
+    private static class MyDriverLoader implements IDriverLoader {
+        @Override public void load(WebDriver driver) throws DriverLoaderException { }
+    }
+
+    private static class MySelector implements ISelector {
+        @Override public Collection<WebElement> select(WebDriver driver) throws SelectorException { return null; }
+    }
+
+    private static class MyElementLoader implements IElementLoader {
+        @Override public void load(WebDriver driver, Collection<WebElement> elements) throws ElementLoaderException { }
+    }
+
+    private static class MyParser implements IParser {
+        @Override public Object parse(WebDriver driver, Collection<WebElement> elements) throws ParserException { return null; }
+    }
+
+    private static class MyProperty implements IProperty {
+        @Override public Object get(Object instance) throws PropertyException { return null; }
+        @Override public void set(Object instance, Object value) throws PropertyException { }
+    }
+
+    @Page(
+        instanceFactory = @InstanceFactory(MyInstanceFactory.class),
+        driverSupplier = @DriverSupplier(MyDriverSupplier.class),
+        driverLoader = @DriverLoader(MyDriverLoader.class)
+    )
+    private static class Person {
+
+        @Element(
+            driverLoader = @DriverLoader(MyDriverLoader.class),
+            selector = @Selector(MySelector.class),
+            elementLoader = @ElementLoader(MyElementLoader.class),
+            parser = @Parser(MyParser.class),
+            property = @Property(MyProperty.class)
+        )
+        public String name;
+
+    }
+
+    private static class NoAnnotationPerson {
+        public String name;
+    }
+
+
+
+    @Test
+    public void buildWithUserConfigTest() throws Exception {
+        Class<?> klass = Person.class;
+        IInstanceFactory instanceFactory = new MyInstanceFactory();
+        IDriverSupplier driverSupplier = new MyDriverSupplier();
+        IDriverLoader driverLoader = new MyDriverLoader();
+        Field field = Person.class.getDeclaredField("name");
+        ISelector selector = new MySelector();
+        IElementLoader elementLoader = new MyElementLoader();
+        IParser parser = new MyParser();
+        IProperty property = new MyProperty();
+
+        ClassConfig classConfig = new ClassConfig(klass, instanceFactory, driverSupplier, driverLoader, new LinkedList<>());
+        FieldConfig fieldConfig = new FieldConfig(field, driverLoader, selector, elementLoader, parser, property);
+        classConfig.getFieldsConfig().add(fieldConfig);
+
+        ConfigBuilder.buildConfig(classConfig);
+
+        assertSame(klass, classConfig.getKlass());
+        assertSame(instanceFactory, classConfig.getInstanceFactory());
+        assertSame(driverSupplier, classConfig.getDriverSupplier());
+        assertSame(driverLoader, classConfig.getDriverLoader());
+        assertSame(field, fieldConfig.getField());
+        assertSame(driverLoader, fieldConfig.getDriverLoader());
+        assertSame(selector, fieldConfig.getSelector());
+        assertSame(elementLoader, fieldConfig.getElementLoader());
+        assertSame(parser, fieldConfig.getParser());
+        assertSame(property, fieldConfig.getProperty());
+    }
+
+    @Test
+    public void buildWithAnnotationConfigTest() throws Exception {
+        ClassConfig classConfig = new ClassConfig(Person.class);
+        FieldConfig fieldConfig = new FieldConfig(Person.class.getDeclaredField("name"));
+        classConfig.getFieldsConfig().add(fieldConfig);
+
+        ConfigBuilder.buildConfig(classConfig);
+
+        assertSame(Person.class, classConfig.getKlass());
+        assertTrue(classConfig.getInstanceFactory() instanceof MyInstanceFactory);
+        assertTrue(classConfig.getDriverSupplier() instanceof MyDriverSupplier);
+        assertTrue(classConfig.getDriverLoader() instanceof MyDriverLoader);
+        assertEquals(Person.class.getDeclaredField("name"), fieldConfig.getField());
+        assertTrue(fieldConfig.getDriverLoader() instanceof MyDriverLoader);
+        assertTrue(fieldConfig.getSelector() instanceof MySelector);
+        assertTrue(fieldConfig.getElementLoader() instanceof MyElementLoader);
+        assertTrue(fieldConfig.getParser() instanceof MyParser);
+        assertTrue(fieldConfig.getProperty() instanceof MyProperty);
+    }
+
+    @Test
+    public void buildDefaultDefaultConfigTest() throws Exception {
+        ClassConfig classConfig = new ClassConfig(NoAnnotationPerson.class);
+        FieldConfig fieldConfig = new FieldConfig(NoAnnotationPerson.class.getDeclaredField("name"));
+        classConfig.getFieldsConfig().add(fieldConfig);
+
+        ConfigBuilder.buildConfig(classConfig);
+
+        assertSame(NoAnnotationPerson.class, classConfig.getKlass());
+        assertTrue(classConfig.getInstanceFactory() instanceof EmptyConstructorInstanceFactory);
+        assertNull(classConfig.getDriverSupplier());
+        assertNotNull(classConfig.getDriverLoader());
+        assertEquals(NoAnnotationPerson.class.getDeclaredField("name"), fieldConfig.getField());
+        assertNotNull(fieldConfig.getDriverLoader());
+        assertTrue(fieldConfig.getSelector() instanceof IdSelector);
+        assertNotNull(fieldConfig.getElementLoader());
+        assertTrue(fieldConfig.getParser() instanceof SingleReferenceStringTextParser);
+        assertTrue(fieldConfig.getProperty() instanceof FieldProperty);
+    }
+
+}
